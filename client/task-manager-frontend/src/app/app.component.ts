84feaa0b3a3dac8a05532f62e1177ed1270e0093
Mkw68Mkw <mkw68mkw@outlook.de>
2025-03-16 21:21:26 +0100

    version1 anzeige

--- client/task-manager-frontend/src/app/app.component.ts
+++ client/task-manager-frontend/src/app/app.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TaskService } from './services/task.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'task-manager-frontend';
+  tasks: any[] = [];
+
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit() {
+    this.loadTasks();
+  }
+
+  loadTasks() {
+    this.taskService.getAllTasks().subscribe({
+      next: (tasks) => this.tasks = tasks,
+      error: (err) => console.error('Error loading tasks:', err)
+    });
+  }
 }
