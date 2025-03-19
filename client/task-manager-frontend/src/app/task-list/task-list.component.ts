57e100deb1a4b04e19de9defab08b17a00237661
Mkw68Mkw <mkw68mkw@outlook.de>
2025-03-19 18:04:07 +0100

    added delete function

--- client/task-manager-frontend/src/app/task-list/task-list.component.ts
+++ client/task-manager-frontend/src/app/task-list/task-list.component.ts
@@ -2,11 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { TaskService } from '../services/task.service';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatIconModule
+  ],
   templateUrl: './task-list.component.html',
   styleUrls: ['./task-list.component.css']
 })
@@ -25,5 +30,16 @@ export class TaskListComponent implements OnInit {
       next: (tasks) => this.tasks = tasks,
       error: (err) => console.error('Error loading tasks:', err)
     });
+  }
+
+  deleteTask(taskId: number) {
+    if(confirm('Are you sure you want to delete this task?')) {
+      this.taskService.deleteTask(taskId).subscribe({
+        next: () => {
+          this.tasks = this.tasks.filter(task => task.id !== taskId);
+        },
+        error: (err) => console.error('Error deleting task:', err)
+      });
+    }
   }
 }
